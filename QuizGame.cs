9a6e1b8c7b22eb87d664ed98302aed8e913cb5a2
MarsaColombo <timbiaou@gmail.com>
2023-11-30 10:19:36 +0100

    Creating a Timer system, making It skip to the next question when the time ends

--- QuizGame.cs
+++ QuizGame.cs
@@ -1,8 +1,12 @@
 using System;
-using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 class QuizGame
 {
+    private static int userChoice;
+
     public static void SelectCategory(QuizData quizData)
     {
         Console.WriteLine("Choisissez une catégorie :");
@@ -45,9 +49,9 @@ class QuizGame
         var options = currentCategory.Options;
         var correctAnswers = currentCategory.CorrectAnswers;
 
-        if (questions == null || questions.Length == 0 || correctAnswers == null)
+        if (questions == null || options == null || correctAnswers == null || questions.Length != options.Length || questions.Length != correctAnswers.Length)
         {
-            Console.WriteLine("Aucune question n'a été récupérée pour cette catégorie.");
+            Console.WriteLine("Erreur : Les données de la catégorie sont invalides.");
             return;
         }
 
@@ -57,22 +61,60 @@ class QuizGame
         {
             Console.WriteLine(questions[i]);
 
-            for (int j = 0; options != null && j < options[i].Length; j++)
+            if (options != null && i < options.Length && options[i] != null)
             {
-                Console.WriteLine(options[i][j]);
+                foreach (var option in options[i])
+                {
+                    Console.WriteLine(option);
+                }
             }
 
-            int userChoice = GetResponse(options?[i]?.Length ?? 0);
+            userChoice = 0;
 
-            isAnswerCorrect[i] = (correctAnswers?[i] == userChoice);
+            // Set up a timer for 10 seconds in a separate thread
+            var cts = new CancellationTokenSource();
+            var stopwatch = new Stopwatch();
+            var timerTask = Task.Run(() => StartTimer(10000, i, stopwatch, cts.Token));
 
-            if (isAnswerCorrect[i])
+            stopwatch.Start(); // Start the stopwatch
+
+            Console.WriteLine("Entrez votre réponse : ");
+
+            // Wait for the user to respond or for the timer to expire
+            GetUserInput(options?[i]?.Length ?? 0, cts);
+
+            // Stop the timer
+            cts.Cancel();
+            stopwatch.Stop(); // Stop the stopwatch
+
+            if (userChoice == 0)
             {
-                Console.WriteLine("Correct !\n");
+                Console.WriteLine($"Aucune réponse donnée à temps ! Temps écoulé : {stopwatch.Elapsed.TotalSeconds} secondes");
+                Console.WriteLine("Appuyer entrer pour continuer");
             }
             else
             {
-                Console.WriteLine($"Incorrect. La réponse correcte était : {options?[i][correctAnswers[i] - 1]}\n");
+                isAnswerCorrect[i] = (correctAnswers != null && i < correctAnswers.Length && correctAnswers[i] == userChoice);
+
+                if (isAnswerCorrect[i])
+                {
+                    Console.WriteLine("\nCorrect !");
+                    Console.WriteLine($"\nTemps écoulé : {stopwatch.Elapsed.TotalSeconds} secondes\n");
+                }
+                else if (options != null && i < options.Length && correctAnswers != null && i < correctAnswers.Length)
+                {
+                    int correctIndex = correctAnswers[i] - 1;
+
+                    if (correctIndex >= 0 && correctIndex < options[i].Length)
+                    {
+                        Console.WriteLine($"\nIncorrect. La réponse correcte était : {options[i][correctIndex]}.");
+                        Console.WriteLine($"\nTemps écoulé : {stopwatch.Elapsed.TotalSeconds} secondes\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nErreur : L'indice de la réponse correcte est hors des limites.\n");
+                    }
+                }
             }
         }
 
@@ -80,7 +122,51 @@ class QuizGame
 
         Console.WriteLine($"Votre score final pour la catégorie '{categoryName}' : {score} sur {questions.Length}");
 
-        Console.ReadLine();
+        Console.ReadLine(); // Attendre une entrée utilisateur à la fin du quiz
+    }
+
+    private static void StartTimer(int milliseconds, int questionIndex, Stopwatch stopwatch, CancellationToken cancellationToken)
+    {
+        try
+        {
+            Task.Delay(milliseconds, cancellationToken).Wait();
+
+            if (userChoice == 0)
+            {
+                Console.WriteLine($"Temps écoulé pour la question {questionIndex + 1} ! Temps total : {stopwatch.Elapsed.TotalSeconds} secondes");
+                userChoice = -1; // Set a special value to indicate that time has expired
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            // Timer was canceled by user input
+        }
+    }
+
+    private static void GetUserInput(int numberOfOptions, CancellationTokenSource cts)
+    {
+        int choice;
+        bool isInvalidChoice;
+
+        do
+        {
+            if (userChoice == -1)
+            {
+                // Skip user input if the time has expired
+                return;
+            }
+
+            isInvalidChoice = !int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > numberOfOptions;
+
+            if (isInvalidChoice)
+            {
+                Console.WriteLine("Erreur : Veuillez entrer un chiffre entre 1 et 4.");
+            }
+            else
+            {
+                userChoice = choice;
+            }
+        } while (isInvalidChoice && !cts.Token.IsCancellationRequested);
     }
 
     public static int GetResponse(int numberOfOptions)
@@ -90,7 +176,6 @@ class QuizGame
 
         do
         {
-            Console.Write($"Entrez le numéro de votre réponse (1-{numberOfOptions}) : ");
             isInvalidChoice = !int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > numberOfOptions;
 
             if (isInvalidChoice)
